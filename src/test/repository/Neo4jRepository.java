1cfbc707da6ecc7f0302e2da2796c94f33cea489
shikha-G <shikha.adarsh.gupta@gmail.com>
2015-09-29 19:44:51 +0530

    minor

--- src/test/repository/Neo4jRepository.java
+++ src/test/repository/Neo4jRepository.java
@@ -3,12 +3,15 @@ package test.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.template.Neo4jTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Neo4jRepository<T> implements GenericRepository<T> {
 
-	//@Autowired
+	@Autowired
+	Neo4jTemplate template;
 	
 	public List<T> findByFields(Map<String, Object> searchParams) {
 		
